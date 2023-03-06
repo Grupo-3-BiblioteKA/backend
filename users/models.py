283279f3d429dcde9acfbb83f07f0a664a931879
Enrico Vieira <enricovieira@hotmail.com>
2023-03-06 16:14:35 -0300

    feat: add User models and permission

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-	first_name = models.CharField(max_length=50)
-	last_name = models.CharField(max_length=50)
-	email = models.EmailField(max_length=254, unique=True)
-	date_unlock = models.DateField(null=True)
+    first_name = models.CharField(max_length=50, default="teste")
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, unique=True)
+    date_unlock = models.DateField(null=True)
