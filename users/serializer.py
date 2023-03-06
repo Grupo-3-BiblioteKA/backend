85e3aa3c0612eecad11e976c33c6fd0ec88d52be
Enrico Vieira <enricovieira@hotmail.com>
2023-03-06 17:20:47 -0300

    feat: add CRUD User

---
+++ users/serializer.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "password",
+            "email",
+            "first_name",
+            "last_name",
+            "is_superuser",
+            "date_unlock",
+        ]
+        extra_kwargs = {"password": {"write_only": True}}
+
+        def create(self, validate_data: dict):
+            return User.objects.create_user(**validate_data)
+
+        def update(self, instance, validated_data):
+            for key, value in validated_data.items():
+                setattr(instance, key, value)
+
+            instance.set_password(instance.password)
+            instance.save()
+
+            return instance
