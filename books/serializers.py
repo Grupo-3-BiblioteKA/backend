5abf21ef6b97eb4eaf9c83c4c3e0eac73fd26617
Anderson-Leandro <andersonmendesleandro@gmail.com>
2023-03-07 14:52:40 -0300

    feat: add relation book followers

--- books/serializers.py
+++ books/serializers.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
 from .models import Book
+from copies.models import Copy
 
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ["id", "name", "author", "year", "sinopsy", "pages"]
+
+    def create(self, validated_data):
+        print(validated_data)
+
+        copies = validated_data.pop("copies")
+
+        new_book = Book.objects.create(**validated_data)
+
+        for _ in range(copies):
+            copie = Copy.objects.create(book_id=new_book.id)
+
+        # import ipdb
+
+        # ipdb.set_trace()
+        return new_book
--- books/views.py
+++ books/views.py
@@ -2,11 +2,24 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from copies.models import Copy
 
 
 class BookView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    def perform_create(self: dict, serializer):
+        copies = self.request.data.pop("copies", 1)
+
+        check_book = Book.objects.filter(**self.request.data).first()
+
+        if check_book:
+            for _ in range(copies):
+                copie = Copy.objects.create(book_id=check_book.id)
+            return check_book
+
+        return serializer.save(copies=copies)
 
 
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
