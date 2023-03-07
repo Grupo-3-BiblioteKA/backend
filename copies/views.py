a44fad392dc069bf9f33f71108c30a254f239ce7
Gabriela <gabrielacamarchiori@gmail.com>
2023-03-07 15:35:24 -0300

    add copy serializer and copy view

--- copies/views.py
+++ copies/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Copy
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from users.permissions import IsCollaborator
+from rest_framework.generics import ListAPIView, RetrieveDestroyAPIView
+from . serializers import CopySerializer
 
-# Create your views here.
+
+class CopyView(ListAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsCollaborator]
+    queryset = Copy.objects.all()
+    serializer_class = CopySerializer
+
+
+class CopyViewDetail(RetrieveDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsCollaborator]
+    queryset = Copy.objects.all()
+    serializer_class = CopySerializer
+
+    lookup_url_kwarg = "copy_id"
+
+
